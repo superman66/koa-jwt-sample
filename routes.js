d202785f9fbe6957059823a0f8c170bc9d566faa
Superman <supermanchc@gmail.com>
2017-07-12 21:56:10 +0800

    Update style

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')();
 const books = require('./controllers/book');
 
 module.exports = function(app){
+    Router.redirect('/', '/book')
     Router.use('/book', books.routes())
     app.use(Router.routes())
 };
